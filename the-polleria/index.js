b5d47f29bc5f20f4ca6071ee9efbee5b4dd755e4
YeisonMC <yeisonmacalupu01@gmail.com>
2024-01-04 18:29:43 -0500

    install cors conection

--- the-polleria/index.js
+++ the-polleria/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import axios from "axios";
+import cors from "cors";
 import polleriaRoutes from "./src/routes/PolleriaRoutes.js";
 import PolleriaHomeRoutes from "./src/routes/PolleriaHomeRoutes.js";
 
 const app = express();
+
 app.listen(3000);
-console.log("Ejuctando en el puerto: 3000");
+app.use(cors());
+console.log("Ejecutando en el puerto: 3000");
 
 app.use(polleriaRoutes);
 app.use(PolleriaHomeRoutes);
--- the-polleria/src/controllers/PolleriaHomeControllers.js
+++ the-polleria/src/controllers/PolleriaHomeControllers.js
@@ -1,6 +1,13 @@
 import { poolConection } from "../conectionDB.js";
 
 export const polleriaHome = async (req, res) => {
-  const [result] = await poolConection.query("SELECT * FROM menu");
-  res.json(result[0]);
+  try {
+    const [results] = await poolConection.query("SELECT * FROM menu");
+    res.json(results);
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+    res
+      .status(500)
+      .json({ error: "Error al obtener datos de la base de datos" });
+  }
 };
